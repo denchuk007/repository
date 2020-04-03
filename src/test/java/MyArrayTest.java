f9efc284d37cb7f9af5e815e9ef9c72dfe48e765
denchuk007 <denisa1999007@gmail.com>
2020-04-03 12:42:49 +0300

    commit

--- src/test/java/MyArrayTest.java
+++ src/test/java/MyArrayTest.java
@@ -1,8 +1,9 @@
 import org.junit.Assert;
+import org.junit.Test;
 
 public class MyArrayTest {
 
-    @org.junit.Test
+    @Test
     public void reverseArray() {
         int[] expectedArray = { 4, 1, 9, 4, 10, 31, 2, 12, 145, 123 };
         int[] actualArray = { 123, 145, 12, 2, 31, 10, 4, 9, 1, 4};
