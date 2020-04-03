4e270f2268aecb043d52be0745a154a50164532b
denchuk007 <denisa1999007@gmail.com>
2020-04-03 12:44:47 +0300

    commit

--- src/test/java/MyArrayTest.java
+++ src/test/java/MyArrayTest.java
@@ -6,7 +6,7 @@ public class MyArrayTest {
     @Test
     public void reverseArray() {
         int[] expectedArray = { 4, 1, 9, 4, 10, 31, 2, 12, 145, 123 };
-        int[] actualArray = { 123, 145, 12, 2, 31, 10, 4, 9, 1, 4};
+        int[] actualArray = { 123, 145, 12, 2, 31, 10, 4, 9, 1, 4, 1231231};
         actualArray = MyArray.reverseArray(actualArray);
         Assert.assertArrayEquals(expectedArray, actualArray);
     }
